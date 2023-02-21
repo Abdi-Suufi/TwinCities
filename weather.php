6bc754411951a77dd262854fe87d083baddad78a
dajaveck <jdavey9@btinternet.com>
2023-02-21 18:50:59 +0000

    added weather

--- weather.php
+++ weather.php
@@ -1,6 +1,6 @@
 <?php
-$apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
-$cityId = 'CITY_ID'; // Replace with the ID of the city you want weather information for
+$apiKey = 'd439f6429d800a5e1e5491375e19b2af'; // Replace with your OpenWeatherMap API key
+$cityId = '1791247'; // Replace with the ID of the city you want weather information for
 
 // Build the API URL
 $url = "http://api.openweathermap.org/data/2.5/weather?id=$cityId&appid=$apiKey&units=metric";
@@ -15,7 +15,7 @@ $description = $weather->weather[0]->description;
 $icon = $weather->weather[0]->icon;
 
 // Build the HTML to display the weather information
-$html = "<h1>Current weather in City Name</h1>";
+$html = "<h1>Current weather in Wuhan</h1>";
 $html .= "<p>Temperature: $temperature &deg;C</p>";
 $html .= "<p>Description: $description</p>";
 $html .= "<img src=\"http://openweathermap.org/img/w/$icon.png\">";
