52518f76b4ed5a5348f8e5a6d07a57d0f1aacfaf
m2-shayannagor <93918232+m2-shayannagor@users.noreply.github.com>
2023-02-24 18:13:17 +0000

    Update landmarks.php
    additonal comments

--- landmarks.php
+++ landmarks.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+//This code creates an HTML document with a title "Landmarks" and an empty div element with an id of landmarkName
+//It also includes JavaScript code that extracts the value of the name parameter from the URL query string and sets it as the content of the div element.
 <html>
 <head>
   <title>Landmarks</title>
