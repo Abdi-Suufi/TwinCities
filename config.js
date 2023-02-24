f3eace4364fa76c52b7242c85fbf1293f7eb50eb
m2-shayannagor <93918232+m2-shayannagor@users.noreply.github.com>
2023-02-24 18:17:11 +0000

    Update config.js
    additional comments

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const config = {
+//This code defines a JavaScript object called config with a property flickrApiKey set to a string value of '3cb96aaa4b49a42b1f147d5bfcf4d9e2' 
+//This is likely used to store the API key for making requests to the Flickr API in a centralized location
     flickrApiKey: '3cb96aaa4b49a42b1f147d5bfcf4d9e2',
     
 };
