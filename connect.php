a43223924b24d21b9d5e668316a6751e85475d3a
m2-shayannagor <93918232+m2-shayannagor@users.noreply.github.com>
2023-03-04 15:45:24 +0000

    Update connect.php
    ADDITIONAL COMMENTS

--- connect.php
+++ connect.php
@@ -1,18 +1,18 @@
 <?php
-
+//PHP code that defines variables used to connect to a MySQL database using the PDO (PHP Data Objects) extension
 $host = '127.0.0.1';
 $dbname = 'mySQL';
 $username = 'root';
 $password = 'password';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
+//This is a PHP array that sets some options for the PDO connection
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
-
+//This is the final part of the PHP code that actually creates a new PDO object and connects to the MySQL database using the DSN, username, password, and options set earlier. 
 try {
     $pdo = new PDO($dsn, $username, $password, $options);
 } catch (PDOException $e) {
