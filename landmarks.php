3845905928132828fd052bdaed6bd0df579abf09
dajaveck <jdavey9@btinternet.com>
2023-03-15 15:45:02 +0000

    flickr api working on landmarks page and forecast data is working

--- landmarks.php
+++ landmarks.php
@@ -2,9 +2,36 @@
 //HTML page that includes a navigation bar and an empty div element with the id of landmarkName.
 <html>
 <head>
-<link rel="stylesheet" href="css/bootstrap.css">
+  <meta charset="utf-8">
   <title>Landmarks</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <style>
+    h1 {
+      font-size: 36px;
+      font-weight: bold;
+      text-align: center;
+    }
+    h2 {
+      font-size: 24px;
+      text-align: center;
+    }
+    p {
+      font-size: 18px;
+      line-height: 1.5;
+      margin: 20px 0;
+    }
+    b {
+      font-size: 20px;
+      font-weight: bold;
+    }
+    .flickrimg {
+      margin: 20px auto;
+      text-align: center;
+    }
+  </style>
 </head>
+<body>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,31 +48,11 @@
     </ul>
   </div>
 </nav>
-<body>
-  <div id="landmarkName"></div>
-//some styles for HTML elements using CSS
-  <style>
-  h1 {
-    font-size: 36px;
-    font-weight: bold;
-    text-align: center;
-  }
-  h2 {
-    font-size: 24px;
-    text-align: center;
-  }
-  p {
-    font-size: 18px;
-    line-height: 1.5;
-    margin: 20px 0;
-  }
-  b {
-    font-size: 20px;
-    font-weight: bold;
-  }
-</style>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
 //This code is a PHP script that fetches data about landmarks from a database and displays it on an HTML page.
-  <?php
+<?php
   $urlParams = $_SERVER['QUERY_STRING'];
   parse_str($urlParams, $params);
   $landmarkName = $params['name'];
@@ -57,22 +64,46 @@
     $stmt = $pdo->prepare('SELECT * FROM sys.Landmarks WHERE name = :name');
     $stmt->bindParam(':name', $landmarkName);
     $stmt->execute();
-} else {
+  } else {
     // If no landmark name is present, select all landmarks
     $stmt = $pdo->query('SELECT * FROM sys.Landmarks');
-}
+  }
 
-// Check if there are any landmarks to display
-if($stmt->rowCount() > 0) {
+  // Check if there are any landmarks to display
+  if($stmt->rowCount() > 0) {
     while ($row = $stmt->fetch()) {
-        echo '<h1>' . $row['name'] . '</h1>'
-            . '<h2>' . $row['city'] . ', ' . $row['country'] . '</h2>'
-            . '<p>' . $row['description'] . '</p>'
-            . '<p><b>Year built:</b> ' . $row['year_built'] . '</p>'
-            . '<p><b>Architect:</b> ' . $row['architect'] . '</p>'
-            . '<p><b>Style:</b> ' . $row['style'] . '</p>';
+
+      $flickrApiKey = '3cb96aaa4b49a42b1f147d5bfcf4d9e2';
+      $url_base = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
+      $url_base .= '&api_key=' . $flickrApiKey;
+      $url_base .= '&page=1';
+      $url_base .= '&per_page=1';
+      $url_base .= '&format=json';
+      $url_base .= '&nojsoncallback=1';
+
+      // Make API request and display photo for the current landmark
+      $landmark_tag = str_replace(' ', '+', $row['name']);
+      $url = $url_base . '&tags=' . $landmark_tag;
+      $response = file_get_contents($url);
+      $data = json_decode($response, true);
+      $photo = $data['photos']['photo'][0];
+      $photo_url = 'https://live.staticflickr.com/' . $photo['server'] . '/' . $photo['id'] . '_' . $photo['secret'] . '.jpg';
+      echo '<div class="card mb-3">';
+      echo '<img class="card-img-top" src="' . $photo_url . '" alt="' . $photo['title'] . '">';
+      echo '<div class="card-body">';
+      echo '<h1 class="card-title">' . $row['name'] . '</h1>';
+      echo '<h2 class="card-subtitle mb-2 text-muted">' . $row['city'] . ', ' . $row['country'] . '</h2>';
+      echo '<p class="card-text">' . $row['description'] . '</p>';
+      echo '<p class="card-text"><b>Year built:</b> ' . $row['year_built'] . '</p>';
+      echo '<p class="card-text"><b>Architect:</b> ' . $row['architect'] . '</p>';
+      echo '<p class="card-text"><b>Style:</b> ' . $row['style'] . '</p>';
+      echo '</div></div>';
+
+      // Flickr API endpoint for searching photos
+
     }
-}
+  }
 ?>
+
 </body>
 </html>
--- weather.php
+++ weather.php
@@ -23,37 +23,74 @@
 <?php
 include 'config.php';
 function extractWeatherData($weather) {
-    $temperature = $weather->main->temp;
-    $description = $weather->weather[0]->description;
-    $icon = $weather->weather[0]->icon;
-    $feelsLike = $weather->main->feels_like;
-    $humidity = $weather->main->humidity;
-    $windSpeed = $weather->wind->speed;
-    return array(
-        'temperature' => $temperature,
-        'description' => $description,
-        'icon' => $icon,
-        'feelsLike' => $feelsLike,
-        'humidity' => $humidity,
-        'windSpeed' => $windSpeed,
-    );
+  $temperature = $weather->main->temp;
+  $description = $weather->weather[0]->description;
+  $icon = $weather->weather[0]->icon;
+  $feelsLike = $weather->main->feels_like;
+  $humidity = $weather->main->humidity;
+  $windSpeed = $weather->wind->speed;
+  return [
+    'temperature' => $temperature,
+    'description' => $description,
+    'icon' => $icon,
+    'feelsLike' => $feelsLike,
+    'humidity' => $humidity,
+    'windSpeed' => $windSpeed,
+  ];
 }
+
+function displayWeatherData($urlManchester, $urlWuhan) {
+  $dataManchester = file_get_contents($urlManchester);
+  $weatherManchester = json_decode($dataManchester);
+
+  $dataWuhan = file_get_contents($urlWuhan);
+  $weatherWuhan = json_decode($dataWuhan);
+
+  $manchesterData = extractWeatherData($weatherManchester);
+  $wuhanData = extractWeatherData($weatherWuhan);
+
+  $html = '<table class="table table-striped">'.
+    '<thead><tr><th>Manchester</th><th>Wuhan</th></tr></thead>'.
+    '<tbody>'.
+      '<tr>'.
+        '<td><h1>Current weather in Manchester<img src="http://openweathermap.org/img/w/' . $manchesterData['icon'] . '.png"></h1>'.
+        '<p>Temperature: ' . $manchesterData['temperature'] . ' &deg;C</p>'.
+        '<p>Description: ' . $manchesterData['description'] . '</p>'.
+        '<p>Feels like: ' . $manchesterData['feelsLike'] . ' &deg;C</p>'.
+        '<p>Humidity: ' . $manchesterData['humidity'] . ' %</p>'.
+        '<p>Wind speed: ' . $manchesterData['windSpeed'] . ' km/h</p></td>'.
+
+        '<td><h1>Current weather in Wuhan<img src="http://openweathermap.org/img/w/' . $wuhanData['icon'] . '.png"></h1>'.
+        '<p>Temperature: ' . $wuhanData['temperature'] . ' &deg;C</p>'.
+        '<p>Description: ' . $wuhanData['description'] . '</p>'.
+        '<p>Feels like: ' . $wuhanData['feelsLike'] . ' &deg;C</p>'.
+        '<p>Humidity: ' . $wuhanData['humidity'] . ' %</p>'.
+        '<p>Wind speed: ' . $wuhanData['windSpeed'] . ' km/h</p></td>'.
+      '</tr>'.
+    '</tbody>'.
+  '</table>';
+
+  echo $html;
+}
+
+// Example usage
+displayWeatherData($urlManchester, $urlWuhan);
+
 
 function getWeatherData($url, $cityName) {
 
   $response = file_get_contents($url);
   $json = json_decode($response);
-  $data = extractWeatherData($json);
-
-  // Extract the relevant weather data for the next 5 days at 3-hour intervals
+  
+  // Extract the relevant weather data for the next 7 days at 3-hour intervals
   $forecastData = array();
-  foreach ($data->list as $forecast) {
+  foreach ($json->list as $forecast) {
       $dateTime = new DateTime($forecast->dt_txt);
-      if ($dateTime->format('H') == '03' && $dateTime < (new DateTime('+5 days'))) {
-          $temperature = round($forecast->main->temp - 273.15, 1);
+      if ($dateTime->format('H') == '03' && $dateTime < (new DateTime('+7 days'))) {
+          $temperature = round($forecast->main->temp, 1);
           $description = $forecast->weather[0]->description;
           $icon = $forecast->weather[0]->icon;
-          $feelsLike = round($forecast->main->feels_like - 273.15, 1);
+          $feelsLike = round($forecast->main->feels_like, 1);
           $humidity = $forecast->main->humidity;
           $windSpeed = $forecast->wind->speed;
           $forecastData[] = array(
@@ -69,63 +106,38 @@ function getWeatherData($url, $cityName) {
   }
 
   // Output the forecast data as HTML
-  $html = '<h2>' . $cityName . '</h2>';
-  $html .= '<table>';
-  $html .= '<tr><th>Date/Time</th><th>Description</th><th>Temperature</th><th>Feels Like</th><th>Humidity</th><th>Wind Speed</th></tr>';
-  foreach ($forecastData as $forecast) {
-      $html .= '<tr>';
-      $html .= '<td>' . $forecast['dateTime'] . '</td>';
-      $html .= '<td>' . $forecast['description'] . '</td>';
-      $html .= '<td>' . $forecast['temperature'] . ' &deg;C</td>';
-      $html .= '<td>' . $forecast['feelsLike'] . ' &deg;C</td>';
-      $html .= '<td>' . $forecast['humidity'] . '%</td>';
-      $html .= '<td>' . $forecast['windSpeed'] . ' m/s</td>';
-      $html .= '</tr>';
-  }
-  $html .= '</table>';
-
-  echo $html;
-}
-
-getWeatherData($urlManchesterForecast, "Manchester");
-getWeatherData($urlWuhanForecast, "Wuhan");
-
-  
-// Retrieve weather data for Manchester and Wuhan
-$dataManchester = file_get_contents($urlManchester);
-$weatherManchester = json_decode($dataManchester);
-
-$dataWuhan = file_get_contents($urlWuhan);
-$weatherWuhan = json_decode($dataWuhan);
-
-// Extract specific weather data using the function
-$manchesterData = extractWeatherData($weatherManchester);
-$wuhanData = extractWeatherData($weatherWuhan);
-
-
-
-
-// Build the HTML string that displays the weather data in a table
-$html = '<table class="table table-striped">';
-$html .= '<thead><tr><th>Manchester</th><th>Wuhan</th></tr></thead>';
-$html .= '<tbody>';
+$html = '<h2>' . $cityName . '</h2>';
+$html .= '<div class="table-responsive">';
+$html .= '<table class="table table-striped">';
+$html .= '<thead>';
 $html .= '<tr>';
-$html .= '<td><h1>Current weather in Manchester<img src="http://openweathermap.org/img/w/'.$manchesterData['icon'].'.png"></h1>';
-$html .= '<p>Temperature: '.$manchesterData['temperature'].' &deg;C</p>';
-$html .= '<p>Description: '.$manchesterData['description'].'</p>';
-$html .= '<p>Feels like: '.$manchesterData['feelsLike'].' &deg;C</p>';
-$html .= '<p>Humidity: '.$manchesterData['humidity'].' %</p>';
-$html .= '<p>Wind speed: '.$manchesterData['windSpeed'].' km/h</p></td>';
-
-$html .= '<td><h1>Current weather in Wuhan<img src="http://openweathermap.org/img/w/'.$wuhanData['icon'].'.png"></h1>';
-$html .= '<p>Temperature: '.$wuhanData['temperature'].' °C</p>';
-$html .= '<p>Description: '.$wuhanData['description'].'</p>';
-$html .= '<p>Feels like: '.$wuhanData['feelsLike'].' °C</p>';
-$html .= '<p>Humidity: '.$wuhanData['humidity'].' %</p>';
-$html .= '<p>Wind speed: '.$wuhanData['windSpeed'].' km/h</p></td>';
+$html .= '<th>Date/Time</th>';
+$html .= '<th>Description</th>';
+$html .= '<th>Temperature (&deg;C)</th>';
+$html .= '<th>Feels Like (&deg;C)</th>';
+$html .= '<th>Humidity (%)</th>';
+$html .= '<th>Wind Speed (m/s)</th>';
 $html .= '</tr>';
+$html .= '</thead>';
+$html .= '<tbody>';
+foreach ($forecastData as $forecast) {
+    $html .= '<tr>';
+    $html .= '<td>' . $forecast['dateTime'] . '</td>';
+    $html .= '<td>' . $forecast['description'] . '</td>';
+    $html .= '<td>' . $forecast['temperature'] . '</td>';
+    $html .= '<td>' . $forecast['feelsLike'] . '</td>';
+    $html .= '<td>' . $forecast['humidity'] . '</td>';
+    $html .= '<td>' . $forecast['windSpeed'] . '</td>';
+    $html .= '</tr>';
+}
 $html .= '</tbody>';
 $html .= '</table>';
+$html .= '</div>';
 
 echo $html;
+}
+
+
+getWeatherData($urlManchesterForecast, "Manchester Forecast");
+getWeatherData($urlWuhanForecast, "Wuhan Forecast");
 ?>
