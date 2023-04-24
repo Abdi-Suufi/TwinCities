0aaa57e2307ab74e954c48d31645201f044f9bad
dajaveck <jdavey9@btinternet.com>
2023-04-24 17:38:02 +0100

    tested code against w3c validation

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 <!--HTML comment -->
 <!--specifies that the document is written in HTML5  -->
 <html lang="en">
-<html>
     <!-- HTML tag  -->
 <head>
    <!--The <head> tag in HTML is used to contain metadata about an HTML document, such as the title, scripts, styles, and other information that is not displayed on the page itself. -->
-    <meta charset="=UTF-8"
-    <meta http-equiv="X-UA-Compatible" content="IE-edge">
-    <meta name="viewport" content="width=device-wodtj, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Twin Cities Project: Manchester | Wuhan</title>
-    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/bootstrap.css">
   <!--The link tag tells the browser where to find the CSS file, which is specified in the href attribute, and the rel attribute indicates that it is a stylesheet. -->
     <script src="js/bootstrap.bundle.js"></script>
   <!-- This is a script tag in HTML that is used to include an external JavaScript file in an HTML document. -->
@@ -20,10 +19,7 @@
   <!-- HTML script tag that loads the Google Maps JavaScript API into an HTML document  -->
   <script src="map.js"></script>
   <!-- HTML script tag that links to an external JavaScript file named "map.js" and loads it into an HTML document.  -->
-</head>
-<!-- The <head> tag in HTML is used to contain metadata about an HTML document, such as the title, scripts, styles, and other information that is not displayed on the page itself  -->
-
-<style>
+  <style>
   nav {
     margin-bottom: 10px;
   }
@@ -48,30 +44,41 @@
     color: red;
   }
   /*Temporary outline */
-  #weather-box {
+  #manchester-weather-box {
     border: 3px solid black;
     margin: 4px;
   }
 
-  #description-box {
+  #wuhan-weather-box {
+    border: 3px solid black;
+    margin: 4px;
+  }
+
+  #manchester-description-box {
     margin-left: 4px;
   }
-</style>
+
+  #wuhan-description-box {
+    margin-left: 4px;
+  }
+ </style>
+<!-- The <head> tag in HTML is used to contain metadata about an HTML document, such as the title, scripts, styles, and other information that is not displayed on the page itself  -->
 
 <!-- Styling the aesthetics of the ("col"umn) class-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">Home</a>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="weather.php">Weather</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Landmarks.php">Landmarks</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="weather.php">Weather</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Landmarks.php">Landmarks</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  
 </head>
 <body>
 
@@ -86,7 +93,7 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
             <h1> Manchester</h1>
             <!--HTML header tag that displays a level one heading on the web page. The text "Manchester" is wrapped in the h1 tags, which denote the largest and most important heading level in HTML. -->
             </div>
-            <div  id="description-box">
+            <div  id="manchester-description-box">
             <p> Manchester is a major city in the northwest of England with a rich industrial heritage. The Castlefield conservation area’s 18th-century canal system recalls the city’s days as a textile powerhouse, and visitors can trace this history at the interactive Museum of Science & Industry. The revitalised Salford Quays dockyards now house the Daniel Libeskind-designed Imperial War Museum North and the Lowry cultural centre.</p>
             <!--  HTML paragraph tag that displays a block of text on the web page. The text describes Manchester as a major city in the northwest of England with a rich industrial heritage, and mentions some of the city's historical and cultural attractions.  -->
             <div id='manchesterMap'></div>
@@ -95,13 +102,10 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
 </div>
 </div>
 </div>
-<div class="col" id="weather-box">
+<div class="col" id="manchester-weather-box">
 <div class="container my-5">
   <div class="row justify-content-center">
   <h1>Weather</h1>
-  <?php
-  /* include 'weather.php'; */
-  ?>
   </div>
 </div>
 </div>
@@ -115,7 +119,7 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
             <h1> Manchester</h1>
             <!--HTML header tag that displays a level one heading on the web page. The text "Manchester" is wrapped in the h1 tags, which denote the largest and most important heading level in HTML. -->
             </div>
-            <div  id="description-box">
+            <div  id="wuhan-description-box">
             <p> Wuhan, the sprawling capital of Central China’s Hubei province, is a commercial center divided by the Yangtze and Han rivers. The city contains many lakes and parks, including expansive, picturesque East Lake. Nearby, the Hubei Provincial Museum displays relics from the Warring States period, including the Marquis Yi of Zeng’s coff in and bronze musical bells from his 5th-century B.C. tomb.</p>
             <!--  HTML paragraph tag that displays a block of text on the web page. The text describes Manchester as a major city in the northwest of England with a rich industrial heritage, and mentions some of the city's historical and cultural attractions.  -->
             <div id='wuhanMap'></div>
@@ -124,7 +128,7 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
 </div>
 </div>
 </div>
-<div class="col" id="weather-box">
+<div class="col" id="wuhan-weather-box">
 <div class="container my-5">
   <div class="row justify-content-center">
   <h1>Weather</h1>
@@ -151,7 +155,7 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
 </div>
 <div class="card" style="width: 18rem;"> 
   <div class="card bg-dark text-white">
-    <img src="https://www.orphanednation.com/wp-content/uploads/2018/12/DSC_8830-1024x678-1024x678.jpg">
+    <img src="https://www.orphanednation.com/wp-content/uploads/2018/12/DSC_8830-1024x678-1024x678.jpg" alt="wuhan-tower">
     <div class="card-body">
       <h5 class="card-title">**FLICKR API POI Name**</h5>
       <p class="card-text">**POI Description**</p>
@@ -162,16 +166,15 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
 
   <div class="card" style="width: 18rem;"> 
     <div class="card bg-dark text-white">
-    <img src="">
     <div class="card-body">
       <h5 class="card-title">**OpenWeatherAPI Metadata**</h5>
       <p class="card-text">**Weather Description**</p>
+      
       <body onload="initMap()">
       <a href="weather.php" class="btn btn-outline-light">Weather Info</a>
     </div>
     </div>
   </div>
-</head>
 </body>
 </html>
  <!-- This ends the HTML document by closing the <html> tag that was opened at the beginning of the document. -->
