3bb3022892a9e9106b381f93f461845f1bede989
dajaveck <jdavey9@btinternet.com>
2023-04-25 20:00:45 +0100

    checked against w3c validation

--- index.php
+++ index.php
@@ -2,18 +2,16 @@
 <!--HTML comment -->
 <!--specifies that the document is written in HTML5  -->
 <html lang="en">
-<html>
-  <body>
     <!-- HTML tag  -->
 <head>
-   <!--The <head> tag in HTML is used to contain metadata about an HTML document, such as the title, scripts, styles, and other information that is not displayed on the page itself. -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Twin Cities Project: Manchester | Wuhan</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+  <!--The <head> tag in HTML is used to contain metadata about an HTML document, such as the title, scripts, styles, and other information that is not displayed on the page itself. -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Twin Cities Project: Manchester | Wuhan</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
   <!--The link tag tells the browser where to find the CSS file, which is specified in the href attribute, and the rel attribute indicates that it is a stylesheet. -->
-    <script src="js/bootstrap.bundle.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
   <!-- This is a script tag in HTML that is used to include an external JavaScript file in an HTML document. -->
   <link rel="page icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/1527/1527807.png">
   <!-- This is an HTML link tag that is used to specify the favicon for a web page.  -->
@@ -21,95 +19,120 @@
   <!-- HTML script tag that loads the Google Maps JavaScript API into an HTML document  -->
   <script src="map.js"></script>
   <!-- HTML script tag that links to an external JavaScript file named "map.js" and loads it into an HTML document.  -->
+  <style>
+    body {
+      background-image: url("https://assets.editorial.aetnd.com/uploads/2015/02/topic-golden-gate-bridge-gettyimages-177770941.jpg");
+      object-fit: cover;
+      background-attachment: fixed;
+      background-color: rgba(0,0,0,0.1);
+      background-blend-mode: lighten;
+      overflow-x: hidden;
+    }
+
+    h1 {
+      color: white;
+    }
+
+    nav {
+      margin-bottom: 10px;
+      top: 0;
+      position: fixed;
+    }
+    .btn {
+      margin: 4px;
+    }
+
+    #manchesterMap {
+      /* setting the margin of 0 auto, width of 500px and height of 300px of the Manchester map and along side with a 1px solid gray border */
+      margin: 0 auto;
+      width: 500px;
+      height: 300px;
+      box-shadow: 0 1px 1px rgba(0,0,0,0.11), 
+              0 2px 2px rgba(0,0,0,0.11), 
+              0 4px 4px rgba(0,0,0,0.11), 
+              0 8px 8px rgba(0,0,0,0.11), 
+              0 16px 16px rgba(0,0,0,0.11), 
+              0 32px 32px rgba(0,0,0,0.11);
+    }
+    
+    #manchesterMap:hover {
+      transform: scale(1.05);
+    }
+    #wuhanMap {
+    /* setting the margin of 0 auto, width of 500px and height of 300px of the Wuhan map and along side with a 1px solid gray border */
+      margin: 0 auto;
+      width: 500px;
+      height: 300px;
+      box-shadow: 0 1px 1px rgba(0,0,0,0.11), 
+              0 2px 2px rgba(0,0,0,0.11), 
+              0 4px 4px rgba(0,0,0,0.11), 
+              0 8px 8px rgba(0,0,0,0.11), 
+              0 16px 16px rgba(0,0,0,0.11), 
+              0 32px 32px rgba(0,0,0,0.11);
+    }
+    h3 {
+      color: red;
+    }
+
+    #wuhan-weather-box {
+      border: 3px solid black;
+      margin: 4px;
+    }
+
+    #manchester-description-box {
+      margin-left: 4px;
+      background-color: white; 
+      box-shadow: 0 1px 1px rgba(0,0,0,0.11), 
+              0 2px 2px rgba(0,0,0,0.11), 
+              0 4px 4px rgba(0,0,0,0.11), 
+              0 8px 8px rgba(0,0,0,0.11), 
+              0 16px 16px rgba(0,0,0,0.11), 
+              0 32px 32px rgba(0,0,0,0.11);
+    }
+
+    #wuhan-description-box {
+      margin-left: 4px;
+      background-color: white; 
+      box-shadow: 0 1px 1px rgba(0,0,0,0.11), 
+              0 2px 2px rgba(0,0,0,0.11), 
+              0 4px 4px rgba(0,0,0,0.11), 
+              0 8px 8px rgba(0,0,0,0.11), 
+              0 16px 16px rgba(0,0,0,0.11), 
+              0 32px 32px rgba(0,0,0,0.11);
+    }
+
+    .card:hover{
+      transform: scale(1.05);
+      transition: 0.2s;
+    }
+  </style>
 </head>
 <!-- The <head> tag in HTML is used to contain metadata about an HTML document, such as the title, scripts, styles, and other information that is not displayed on the page itself  -->
 
-<style>
-  body {
-    background-image: url("https://assets.editorial.aetnd.com/uploads/2015/02/topic-golden-gate-bridge-gettyimages-177770941.jpg");
-    object-fit: cover;
-    background-attachment: fixed;
-    background-color: rgba(0,0,0,0.1);
-    background-blend-mode: lighten;
-    overflow-x: hidden;
-  }
-
-  h1 {
-    color: white;
-  }
-
-  nav {
-    margin-bottom: 10px;
-    top: 0;
-    position: fixed;
-  }
-   .btn {
-    margin: 4px;
-   }
-
-    #manchesterMap {
-    /* setting the margin of 0 auto, width of 500px and height of 300px of the Manchester map and along side with a 1px solid gray border */
-    margin: 0 auto;
-    width: 500px;
-    height: 300px;
-  }
-  
-  #manchesterMap:hover {
-    transform: scale(1.05);
-  }
-  #wuhanMap {
-   /* setting the margin of 0 auto, width of 500px and height of 300px of the Wuhan map and along side with a 1px solid gray border */
-    margin: 0 auto;
-    width: 500px;
-    height: 300px;
-  }
-  h3 {
-    color: red;
-  }
-
-  #wuhan-weather-box {
-    border: 3px solid black;
-    margin: 4px;
-  }
-
-  #manchester-description-box {
-    margin-left: 4px;
-    /* background-color: lightgrey; */
-  }
-
-  #wuhan-description-box {
-    margin-left: 4px;
-  }
-
-  .card:hover{
-    transform: scale(1.05);
-    transition: 0.2s;
-}
-</style>
-
+<body onload="initMap()">
 <!-- Styling the aesthetics of the ("col"umn) class-->
 <div class="fixed-top">
-<nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
-  <a class="navbar-brand" href="index.php" style="margin-left: 2px;">Home</a>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link black" href="weather.php">Weather</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Landmarks.php">Landmarks</a>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
+    <a class="navbar-brand" href="index.php" style="margin-left: 2px;">Home</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link black" href="weather.php">Weather</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Landmarks.php">Landmarks</a>
+        </li>
+      </ul>
     </div>
-  </div>
-</nav>
+  </nav>
+</div>
 
 <!-- <a type="button" href="flickr.php"
 class="btn btn-outline-dark btn-md">flickr temporary</a> -->
 
 <div class="row">
 <div class="col">
-    <div class="container my-5" id="city-info-box">
+    <div class="container my-5" id="manchester-info-box">
         <div class="row justify-content-center">
           <div class="center">
             <h1> Manchester</h1>
@@ -129,7 +152,7 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
 
 <div class="row">
 <div class="col">
-    <div class="container my-5" id="city-info-box">
+    <div class="container my-5" id="wuhan-info-box">
         <div class="row justify-content-center">
           <div class="center">
             <h1> Wuhan</h1>
@@ -170,7 +193,6 @@ class="btn btn-outline-dark btn-md">flickr temporary</a> -->
   </div>
   </div>
   </div>
-      <body onload="initMap()">
 </body>
 </html>
  <!-- This ends the HTML document by closing the <html> tag that was opened at the beginning of the document. -->
