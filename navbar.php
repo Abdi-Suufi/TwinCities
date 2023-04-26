7c1a39c3f5c01151507d721ad1c04c7951f0d641
Abdi-Suufi <abdirahman2.suufi@live.uwe.ac.uk>
2023-04-26 12:38:00 +0100

    nav file

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-">
+  <a class="navbar-brand" href="index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="weather.php">Weather</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Landmarks.php">Landmarks</a>
+      </li>
+    </ul>
+  </div>
+</nav>
